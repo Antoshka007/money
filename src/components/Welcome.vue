cde173967fc095b7b3d2d8711e6cf9df952d0dc8
Vahrushev Anton <toha.vahrushev2010@yandex.ru>
2019-09-17 17:17:37 +0300

    welcome component

---
+++ src/components/Welcome.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="custom-background">
+        <div class="custom-background__inner">
+            <h1 class="text-center display-1 mb-10 blue-grey--text text--darken-2">Добро пожаловать в MyMoney!</h1>
+
+            <p class="custom-description blue-grey--text text--darken-4">
+                Данный сервис поможет Вам в управлении личными финансами. Записывайте свои доходы
+                и расходы, распределяя их по категориям, а MyMoney наглядным образом визуализирует все данные,
+                чтобы вы хорошо понимали своё финансовое положение.
+            </p>
+        </div>
+    </div>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,6 @@
 <template>
     <div>
-        <div v-if="!isLoggedIn" class="custom-background">
-            <div class="custom-background__inner">
-                <h1 class="text-center display-1 mb-10 blue-grey--text text--darken-2">Добро пожаловать в MyMoney!</h1>
-
-                <p class="custom-description blue-grey--text text--darken-4">
-                    Данный сервис поможет Вам в управлении личными финансами. Записывайте свои доходы
-                    и расходы, распределяя их по категориям, а MyMoney наглядным образом визуализирует все данные,
-                    чтобы вы хорошо понимали своё финансовое положение.
-                </p>
-            </div>
-        </div>
+        <Welcome v-if="!isLoggedIn"/>
 
         <v-container v-if="isLoggedIn">
             <v-row>
@@ -138,11 +128,12 @@
 	import Form from '@/components/Form';
 	import Item from '@/components/Item';
 	import Pie from '@/components/Pie';
+	import Welcome from '@/components/Welcome';
 
 	const db = firebase.firestore();
 
 	export default {
-		components: { Pie, Form, Item },
+		components: { Pie, Form, Item, Welcome },
 		data() {
 			return {
 				isLoggedIn: false,
