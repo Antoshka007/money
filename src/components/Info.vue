654a7d66e4bf4cd3497e4ce7144048dfeafa2a1c
Vahrushev Anton <toha.vahrushev2010@yandex.ru>
2019-09-17 15:11:09 +0300

    info component

---
+++ src/components/Info.vue
@@ -0,0 +1,84 @@
+<template>
+    <v-card>
+        <v-card-title>Информация о проекте</v-card-title>
+
+        <v-list>
+            <v-list-item>
+                <v-list-item-content>
+                    <v-list-item-title>
+                        Vue.js
+                    </v-list-item-title>
+                    <v-list-item-subtitle>
+                        JavaScript фреймворк
+                    </v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item>
+                <v-list-item-content>
+                    <v-list-item-title>
+                        Vuetify
+                    </v-list-item-title>
+                    <v-list-item-subtitle>
+                        Библиотека компонентов
+                    </v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item>
+                <v-list-item-content>
+                    <v-list-item-title>
+                        Chart.js & vue-chartjs
+                    </v-list-item-title>
+                    <v-list-item-subtitle>
+                        Библиотека для постороения графиков и диаграмм
+                    </v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item>
+                <v-list-item-content>
+                    <v-list-item-title>
+                        Firebase
+                    </v-list-item-title>
+                    <v-list-item-subtitle>
+                        NoSQL база данных
+                    </v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item>
+                <v-list-item-content>
+                    <v-list-item-title>
+                        Vue CLI
+                    </v-list-item-title>
+                    <v-list-item-subtitle>
+                        CLI
+                    </v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item>
+                <v-list-item-content>
+                    <v-list-item-title>
+                        Git
+                    </v-list-item-title>
+                    <v-list-item-subtitle>
+                        Система контроля версий
+                    </v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item>
+                <v-list-item-content>
+                    <v-list-item-title>
+                        ESLint
+                    </v-list-item-title>
+                    <v-list-item-subtitle>
+                        JavaScript линтер
+                    </v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
+        </v-list>
+    </v-card>
+</template>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -9,88 +9,7 @@
                         </span>
                     </template>
 
-                    <v-card>
-                        <v-card-title>Информация о проекте</v-card-title>
-
-                        <v-list>
-                            <v-list-item>
-                                <v-list-item-content>
-                                    <v-list-item-title>
-                                        Vue.js
-                                    </v-list-item-title>
-                                    <v-list-item-subtitle>
-                                        JavaScript фреймворк
-                                    </v-list-item-subtitle>
-                                </v-list-item-content>
-                            </v-list-item>
-
-                            <v-list-item>
-                                <v-list-item-content>
-                                    <v-list-item-title>
-                                        Vuetify
-                                    </v-list-item-title>
-                                    <v-list-item-subtitle>
-                                        Библиотека компонентов
-                                    </v-list-item-subtitle>
-                                </v-list-item-content>
-                            </v-list-item>
-
-                            <v-list-item>
-                                <v-list-item-content>
-                                    <v-list-item-title>
-                                        Chart.js & vue-chartjs
-                                    </v-list-item-title>
-                                    <v-list-item-subtitle>
-                                        Библиотека для постороения графиков и диаграмм
-                                    </v-list-item-subtitle>
-                                </v-list-item-content>
-                            </v-list-item>
-
-                            <v-list-item>
-                                <v-list-item-content>
-                                    <v-list-item-title>
-                                        Firebase
-                                    </v-list-item-title>
-                                    <v-list-item-subtitle>
-                                        NoSQL база данных
-                                    </v-list-item-subtitle>
-                                </v-list-item-content>
-                            </v-list-item>
-
-                            <v-list-item>
-                                <v-list-item-content>
-                                    <v-list-item-title>
-                                        Vue CLI
-                                    </v-list-item-title>
-                                    <v-list-item-subtitle>
-                                        CLI
-                                    </v-list-item-subtitle>
-                                </v-list-item-content>
-                            </v-list-item>
-
-                            <v-list-item>
-                                <v-list-item-content>
-                                    <v-list-item-title>
-                                        Git
-                                    </v-list-item-title>
-                                    <v-list-item-subtitle>
-                                        Система контроля версий
-                                    </v-list-item-subtitle>
-                                </v-list-item-content>
-                            </v-list-item>
-
-                            <v-list-item>
-                                <v-list-item-content>
-                                    <v-list-item-title>
-                                        ESLint
-                                    </v-list-item-title>
-                                    <v-list-item-subtitle>
-                                        JavaScript линтер
-                                    </v-list-item-subtitle>
-                                </v-list-item-content>
-                            </v-list-item>
-                        </v-list>
-                    </v-card>
+                    <Info/>
                 </v-dialog>
             </v-toolbar-title>
 
@@ -178,10 +97,12 @@
 </template>
 
 <script>
+    import Info from '@/components/Info';
     import firebase from 'firebase/app';
 	import 'firebase/auth';
 
     export default {
+    	components: { Info },
         data() {
         	return {
         		isLoggedIn: false,
