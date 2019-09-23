d806c56b123bb7dfb5916822e10ac391968926ca
Vahrushev Anton <toha.vahrushev2010@yandex.ru>
2019-09-23 20:24:42 +0300

    welcome styles

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -271,44 +271,9 @@
 </script>
 
 <style>
-    .custom-background {
-        display: flex;
-        align-items: flex-start;
-        height: 100vh;
-        margin-top: -56px;
-        padding-top: 56px;
-
-        background-image: url(../assets/background.jpg);
-        background-size: cover;
-        background-repeat: no-repeat;
-    }
-
-    .custom-background__inner {
-        max-width: 700px;
-        padding: 50px;
-        margin-left: auto;
-        margin-right: auto;
-        margin-top: 100px;
-
-        background-color: rgba(255, 255, 255, 0.8);
-    }
-
-    .custom-description {
-        max-width: 600px;
-        margin-left: auto;
-        margin-right: auto;
-    }
-
     .custom-chart {
         margin-left: auto;
         margin-right: auto;
         max-width: 300px;
-    }
-
-    @media screen and (min-width: 960px) {
-        .custom-background {
-            margin-top: -64px;
-            padding-top: 64px;
-        }
     }
 </style>
